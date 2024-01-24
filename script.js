570abe7ecc199cf9fe6aa2e47f66513c0f42c64a
elinaavon <140659627+elinaavon@users.noreply.github.com>
2024-01-24 14:52:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ order.addEventListener("click", () => {
     let random10OffCode = generateRandomCodeFor10Off();
 
     // Display the code
-    alert("10% Off Your Fist Order Code:\n" + "<strong>" + random10OffCode + "<strong>");
+    alert("<strong>" + "10% Off Your Fist Order Code:\n" + random10OffCode + "</strong>");
 
     let data = {
         name: name,
